e596b14a39f17554601bfccd692ca61a5e01a093
andrevalos88 <andres.mejia.arevalo.88@gmail.com>
2021-10-16 10:53:04 -0600

    Initial Commit

---
+++ MyPlaceholderJson/MyPlaceholderJsonApp.swift
@@ -0,0 +1,20 @@
+//
+//  MyPlaceholderJsonApp.swift
+//  MyPlaceholderJson
+//
+//  Created by iOS Development on 10/16/21.
+//
+
+import SwiftUI
+
+@main
+struct MyPlaceholderJsonApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
